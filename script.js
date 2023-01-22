4c150f7a4bc1778b7324aa0b8f9a5e87a8187f86
Koshkaflo <glebovanna94@gmail.com>
2023-01-22 19:12:10 +0300

    add new code -99bottles

--- script.js
+++ script.js
@@ -15,10 +15,27 @@ while (scoops > 0) {
 document.write("Life without ice cream isn't the same<br>");
 
 var i = 0;
-var name = "Joe";
+var name1 = "Joe";
 while (i < 2) {
     document.write("Happy Birthday to you. <br>");
     i = i + 1;
 }
-document.write("Happy Birthday dear" + name + ", <br>");
+document.write("Happy Birthday dear" + name1 + ", <br>");
 document.write("Happy Birthday to you. <br>");
+
+//console.log
+
+var word = "bottles";
+var count = 99;
+while (count > 0) {
+    console.log(count + " " + word + " of bear on the wall");
+    console.log(count + " " + word + " of bear,");
+    console.log("Take one down, pass it around,");
+    count = count - 1;
+    if (count > 0) {
+        console.log(count + " " + word + " of beer on the wall.");
+    } else {
+        console.log("No more " + word + " of beer on the wall.");
+    }
+
+}
