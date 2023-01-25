f14f1915fd6d699eb390c8cfd5050db92bf4cae9
Koshkaflo <glebovanna94@gmail.com>
2023-01-25 19:13:49 +0300

    add function bark()

--- script.js
+++ script.js
@@ -39,3 +39,19 @@ while (count > 0) {
     }
 
 }
+
+// function
+
+
+
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + ' says WOOF WOOF');
+    } else {
+        console.log(name + ' says woof woof');
+    }
+}
+bark('rover', 23);
+bark('spot', 13);
+bark('spike', 53);
+bark('lady', 17);
