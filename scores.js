6b8e14dcc9880e27407b2b12986554c6e3b13d36
Koshkaflo <glebovanna94@gmail.com>
2023-02-09 20:00:59 +0300

    add HighScore

--- scores.js
+++ scores.js
@@ -1,6 +1,12 @@
 const scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 66, 61, 44, 53, 18, 31];
+let highScore = 0;
 let output;
 for (let i = 0; i < scores.length; i++) {
     output = 'Bubble solution #' + i + ' score: ' + scores[i];
     console.log(output);
+    if (scores[i] > highScore) {
+        highScore = scores[i];
+    };
 };
+console.log('Bubbles tests: ' + scores.length);
+console.log('Highest bubble score: ' + highScore);
