0b0eccccece4cb2f84df354b29a3195bc04e7c08
Koshkaflo <glebovanna94@gmail.com>
2023-02-09 19:46:36 +0300

    changed l-3

--- scores.js
+++ scores.js
@@ -1,6 +1,6 @@
 const scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 66, 61, 44, 53, 18, 31];
 let output;
-for (let i = 0; i < scores.length; i = i + 1) {
+for (let i = 0; i < scores.length; i++) {
     output = 'Bubble solution #' + i + ' score: ' + scores[i];
     console.log(output);
 };
