970e5291982e13225f79f250d18a50bb8aeb0674
Koshkaflo <glebovanna94@gmail.com>
2023-02-14 21:03:49 +0300

    add some objects

---
+++ objects.js
@@ -0,0 +1,51 @@
+// objects basics
+
+let chevy = {
+    make: 'Chevy',
+    model: 'Bel Air',
+    year: 1957,
+    color: 'red',
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+};
+
+let cadiDev = {
+    make: 'Cadillac',
+    model: 'Coupe Deville',
+    type: 'coupe',
+    year: 1949,
+    engine: '331 CID V8',
+    color: 'green',
+    price: 3.496,
+    passengers: 4
+};
+
+let cadi = {
+    make: 'GM',
+    model: 'Cadillac',
+    year: 1955,
+    color: 'tan',
+    passengers: 5,
+    convertible: false,
+    mileage: 12892
+};
+
+// magnets
+
+let dog = {
+    name: 'Fido',
+    weight: 20.2,
+    age: 4,
+    breed: 'mixed',
+    activity: 'fetch balls'
+};
+let bark1;
+if (dog.weight > 20) {
+    bark1 = 'WOOF WOOF';
+} else {
+    bark1 = 'woof woof';
+};
+let speak = dog.name + ' says ' + bark1 + ' when he wants to ' + dog.activity;
+console.log(speak);
+
