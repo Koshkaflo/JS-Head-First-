1595ddcad33a2301734da7d2791301a2db0f45ee
Koshkaflo <glebovanna94@gmail.com>
2023-02-21 15:38:30 +0300

    add "setAttribute" for planet

--- planets/planets.js
+++ planets/planets.js
@@ -1,6 +1,7 @@
 function init() {
 let planet = document.getElementById('greenplanet');
 planet.innerHTML = 'Red Alert: hit by phaser fire!';
+planet.setAttribute('class', 'redtext');
 };
 
 window.onload = init;
