0bf16c6f78be764e2ba3bca5cd0daf767999a588
Koshkaflo <glebovanna94@gmail.com>
2023-03-15 15:56:31 +0300

    add fn compareSold and printProducts

--- webcola/script.js
+++ webcola/script.js
@@ -8,3 +8,24 @@ let products = [
     {name: 'root beer', calories: 200, color: 'caramel', sold: 9909},
     {name: 'water', calories: 0, color: 'clear', sold: 62123},
 ];
+
+// sort by sold
+function compareSold(colaA, colaB) {
+    if (colaA.sold > colaB.sold) {
+        return 1;
+    } else if (colaA.sold === colaB.sold) {
+        return 0;
+    } else {
+        return -1;
+    };
+};
+
+// print list pf products
+function printProducts (products) {
+    for ( let i = 0; i < products.length; i++) {
+        console.log('Name: ' + products[i].name + ', Calories: ' + products[i].calories + ', Color: ' + products[i].color + ', Sold: ' + products[i].sold);    
+    };
+};
+
+products.sort(compareSold);
+printProducts(products);
