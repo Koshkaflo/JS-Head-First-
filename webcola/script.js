a5ea4e590b01243552dca5b35eb98c31cd364011
Koshkaflo <glebovanna94@gmail.com>
2023-03-15 14:21:47 +0300

    add arr products

---
+++ webcola/script.js
@@ -0,0 +1,10 @@
+let products = [
+    {name: 'grapefruit', calories: 170, color: 'red', sold: 8200},
+    {name: 'orange', calories: 160, color: 'orange', sold: 12101},
+    {name: 'cola', calories: 210, color: 'caramel', sold: 25412},
+    {name: 'diet cola', calories: 0, color: 'caramel', sold: 43922},
+    {name: 'lemon', calories: 200, color: 'clear', sold: 14983},
+    {name: 'raspberry', calories: 180, color: 'pink', sold: 9427},
+    {name: 'root beer', calories: 200, color: 'caramel', sold: 9909},
+    {name: 'water', calories: 0, color: 'clear', sold: 62123},
+];
